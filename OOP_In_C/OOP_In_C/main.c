b2e929733d7775e0b95d87c05e605c2dd4cbcb1f
sxd <suxinde2009@126.com>
2015-05-05 01:34:50 +0800

    Init

---
+++ OOP_In_C/OOP_In_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OOP_In_C
+//
+//  Created by su xinde on 15/5/5.
+//  Copyright (c) 2015年 su xinde. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
